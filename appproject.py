0fe9972274c452c2c61f82052f622d1fbd41c221
chloe posthuma-coelho <96831510+chloeposthumacoelho@users.noreply.github.com>
2023-03-20 18:50:34 -0400

    Update appproject.py

--- appproject.py
+++ appproject.py
@@ -1,115 +1,42 @@
-import streamlit as st 
-import streamlit.components.v1 as stc
-import requests 
-
-base_url = "https://jobs.github.com/positions.json?description={}&location={miami}"
-
-# Fxn to Retrieve Data
-def get_data(url):
-	resp = requests.get(url)
-	return resp.json()
-
-
-JOB_HTML_TEMPLATE = """
-<div style="width:100%;height:100%;margin:1px;padding:5px;position:relative;border-radius:5px;border-bottom-right-radius: 10px;
-box-shadow:0 0 1px 1px #eee; background-color: #31333F;
-  border-left: 5px solid #6c6c6c;color:white;">
-<h4>{}</h4>
-<h4>{}</h4>
-<h5>{}</h5>
-<h6>{}</h6>
-</div>
-"""
-
-JOB_DES_HTML_TEMPLATE = """
-<div style='color:#fff'>
-{}
-</div>
-"""
+# TODO continue studying this API https://docs.streamlit.io/en/stable/api.html#display-charts
+import pandas as pd
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import json_lines
+sns.set()
+from projects_vis import *
+from notion_api import *
 
 
-def main():
-	menu = ["Home","About"]
-	choice = st.sidebar.selectbox("Menu",menu)
+# Caches automatically
+@st.cache
+def load_data():
+    nsync = NotionSync()
+    data = nsync.query_databases()
+    projects = nsync.get_projects_titles(data)
+    projects_data,dates = nsync.get_projects_data(data,projects)
+    df = setupProjectsDf(projects_data,dates)
+    return df
 
-	st.title("DevDeeds -Search Jobs")
+    
 
-	if choice == "Home":
-		st.subheader("Home")
-
-		# Nav  Search Form
-		with st.form(key='searchform'):
-			nav1,nav2,nav3 = st.beta_columns([3,2,1])
-
-			with nav1:
-				search_term = st.text_input("Search Job")
-			with nav2:
-				location = st.text_input("Location: miami- FL")
-
-			with nav3:
-				st.text("Search ")
-				submit_search = st.form_submit_button(label='Search')
-
-		st.success("You searched for {} in {}".format(search_term,location))
-
-		# Results
-		col1, col2 = st.beta_columns([2,1])
-
-		with col1:
-			if submit_search:
-				# Create Search Query
-				search_url = base_url.format(search_term,location)
-				# st.write(search_url)
-				data = get_data(search_url)
-
-				# Number of Results
-				num_of_results = len(data)
-				st.subheader("Showing {} jobs".format(num_of_results))
-				# st.write(data)
-		
-
-				for i in data:
-					job_title = i['title']
-					job_location = i['location']
-					company = i['company']
-					company_url = i['company_url']
-					job_post_date = i['created_at']
-					job_desc = i['description']
-					job_howtoapply = i['how_to_apply']
-					st.markdown(JOB_HTML_TEMPLATE.format(job_title,company,job_location,job_post_date),
-						unsafe_allow_html=True)
-
-					# Description
-					with st.beta_expander("Description"):
-						stc.html(JOB_DES_HTML_TEMPLATE.format(job_desc),scrolling=True)
-
-					# How to Apply
-					with st.beta_expander("How To Apply"):
-						# stc.html(job_howtoapply) # For White Theme
-						stc.html(JOB_DES_HTML_TEMPLATE.format(job_howtoapply),scrolling=True) # For Dark Theme
+df = load_data()
+i=1
+for p in df.columns:
+    if p!="date" and p!="Name":
+        df[p] = df[p].apply(lambda x: i if x==True else -0.5)
+        i+=1
 
 
-		with col2:
-			with st.form(key='email_form'):
-				st.write("Be the first to get new jobs info")
-				email = st.text_input("Email")
-
-				submit_email = st.form_submit_button(label='Subscribe')
-
-				if submit_email:
-					st.success("A message was sent to {}".format(email))
-
-
-
-
-
-
-
-	else:
-		st.subheader("About")
-
-
-
-
-if __name__ == '__main__':
-	main()
+st.title("Project Tracker")
+# Set up: load and set up the data and write loading message
+data_load_state = st.text('Loading data...')
+data_load_state.text('Loading data...done!')
+# Plotting Projects Stats
+st.subheader("Project Tracker")
+project_time_spents = projects_pie_chart(df)
+st.write(project_time_spents)
+project_events = projects_scatter_plot(df)
+st.write(project_events)
