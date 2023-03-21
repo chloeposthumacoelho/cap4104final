2a28cb8e27419cf9e8a6086f479d2156e6a6e208
chloe posthuma-coelho <96831510+chloeposthumacoelho@users.noreply.github.com>
2023-03-21 18:26:01 -0400

    Update appproject.py

--- appproject.py
+++ appproject.py
@@ -2,9 +2,10 @@ import streamlit as st
 import requests
 import pycountry
 from api import apiKEY
+apiKey='0a54baab3c394337aa46d1ea286cfb24'
 
-st.title('news app')
-st.radio('news category chsen:',('technology', 'poliutics', 'sports''business'))
+st.title('news app final CAP 4104 ')
+st.radio('news category chosen:',('technology', 'poliutics', 'sports''business'))
 col1, col2= st.columns(2)    ,
 with col1:
     user= st.text_input('enter country')
@@ -12,8 +13,10 @@ with col2:
     category= st.radio('news category chsen:',('technology', 'poliutics', 'sports''business'))
     btn= st.button('enter')
 if btn:
+    #asking user for country input, ex us for us news and turns it into alpha_2 through user method
     country= pycountry.countries.get(name=user).alpha_2
-    url= f"https:./newsapi.org/v2/top-headlines?country={country}&apiKey={apiKey}"
+    url= f"https:./newsapi.org/v2/top-headlines?country=us&apiKey=0a54baab3c394337aa46d1ea286cfb24"
+   
     r= requests.get(url)
     r= r.json()
     articles= r['articles']
