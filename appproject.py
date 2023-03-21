202bdaae815d2b57d2ab2adee818f27c713b2b99
chloe posthuma-coelho <96831510+chloeposthumacoelho@users.noreply.github.com>
2023-03-21 12:34:21 -0400

    Update appproject.py

--- appproject.py
+++ appproject.py
@@ -1,36 +1,31 @@
-# Modules
-import streamlit as st
+#import streamlit as st
 import requests
-
-# Notion Token
 token='secret_jmaR7EWtxKMWPB8GvTnx1yjwbPzUUNW9QrGlouTLIX9'
 databaseid='064c9fddf0f74cf786d0e16ff2ae608f'
 
-# Headers
-headers = {
-    "Authorization": "Bearer " + token,
-    "Content-Type": "application/json",
-    "Notion-Version": "2021-05-13"
+headers= {
+
+"Authorization": "bearer" + token,
+"Content-Type": "application/json",
+"Notion-Version": "2021-05-13"
 }
+#query from database
 
-# Functions
-# Database Query
-def queryDatabase(databaseId, headers):
-    readUrl = f"https://api.notion.com/v1/databases/{databaseId}/query"
-    res = requests.request("POST", readUrl, headers=headers)
-    data = res.json()
+
+def queryDatabase(databaseID, headers):
+    #readUrl= f"https://api.notion.com/v1/databases/(databaseID)/query"
+    readUrl = f"https://api.notion.com/v1/databases/(databaseID)/query"
+    res= requests.request("POST", readUrl, headers=headers)
+    data= res.json()
+    return res,data
+
+#retrieving from database
+def retrieveDatabase(databaseID, headers):
+    readUrl= f"https://api.notion.com/v1/databases/{databaseID}"
+    res= requests.request("GET", readUrl, headers=headers)
+    data= res.json()
     return res , data
-
-# Database retrieve
-def retrieveDatabase(databaseId, headers):
-    readUrl = f"https://api.notion.com/v1/databases/{databaseId}"
-    res = requests.request("GET", readUrl, headers=headers)
-    data = res.json()
-    return res , data
-
-# Calling the function
-res , data = queryDatabase(databaseID,headers)
-
-# Dump onto Streamlit
+#
+res, data= queryDatabase(databaseID, headers)
 st.write(res.status_code)
 st.json(data)
