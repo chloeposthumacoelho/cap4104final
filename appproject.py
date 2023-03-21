05eaa8c5fccbfcc6293a8fcc628ed892865ef8e4
chloe posthuma-coelho <96831510+chloeposthumacoelho@users.noreply.github.com>
2023-03-21 12:10:23 -0400

    Update appproject.py

--- appproject.py
+++ appproject.py
@@ -1,42 +1,31 @@
-# TODO continue studying this API https://docs.streamlit.io/en/stable/api.html#display-charts
-import pandas as pd
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import json_lines
-sns.set()
-from projects_vis import *
-from notion_api import *
+import requests
+token='secret_jmaR7EWtxKMWPB8GvTnx1yjwbPzUUNW9QrGlouTLIX9'
+databaseid='064c9fddf0f74cf786d0e16ff2ae608f'
+
+headers= {
+
+"Authorization": "bearer" + token,
+"Content-Type": "application/json",
+"Notion-Version": "2021-05-13"
+}
+#query from database
 
 
-# Caches automatically
-@st.cache
-def load_data():
-    nsync = NotionSync()
-    data = nsync.query_databases()
-    projects = nsync.get_projects_titles(data)
-    projects_data,dates = nsync.get_projects_data(data,projects)
-    df = setupProjectsDf(projects_data,dates)
-    return df
+def queryDatabase(databaseID, headers):
+    #readUrl= f"https://api.notion.com/v1/databases/(databaseID)/query"
+    readUrl = f"https://api.notion.com/v1/databases/(databaseID)/query"
+    res= requests.request("POST", readUrl, headers=headers)
+    data= res.json()
+    return res,data
 
-    
-
-df = load_data()
-i=1
-for p in df.columns:
-    if p!="date" and p!="Name":
-        df[p] = df[p].apply(lambda x: i if x==True else -0.5)
-        i+=1
-
-
-st.title("Project Tracker")
-# Set up: load and set up the data and write loading message
-data_load_state = st.text('Loading data...')
-data_load_state.text('Loading data...done!')
-# Plotting Projects Stats
-st.subheader("Project Tracker")
-project_time_spents = projects_pie_chart(df)
-st.write(project_time_spents)
-project_events = projects_scatter_plot(df)
-st.write(project_events)
+#retrieving from database
+def retrieveDatabase(databaseID, headers):
+    readUrl= f"https://api.notion.com/v1/databases/{databaseID}"
+    res= requests.request("GET", readUrl, headers=headers)
+    data= res.json()
+    return res , data
+#
+res, data= queryDatabase(databaseID, headers)
+st.write(res.status_code)
+st.json(data)
